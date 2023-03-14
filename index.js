e125d9abc53ef86cee2aeda67bb4140cc8903c27
Harrison Shapiro <heshapiro.hs@gmail.com>
2023-03-14 18:31:24 -0400

    commit tree 5e4166b2965cb72b0405cb6035d22cd34a5caba1

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+const a = "cat name"
+function destructivelyAppendCat(a) {
+    cats.push(a)
+}
+console.log(cats)
+
+function destructivelyPrependCat (b) {
+    cats.unshift(b)
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+function appendCat(c) {
+    return [...cats, c]
+}
+function prependCat(d) {
+    return [d, ...cats]
+}
+function removeLastCat() {
+    return cats.slice(0, -1)
+}
+function removeFirstCat() {
+    return cats.slice(1)
+}
